6a8caa4f2909715049f1a2cc873680857aa56dd3
Peter Irvine <peirvine@gmail.com>
2020-04-24 20:25:08 -0500

    adding slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css'
-import { Toolbar, AppBar, IconButton, Button, Typography } from '@material-ui/core';
-import { MenuIcon } from '@material-ui/icons'
+import { Toolbar, AppBar, IconButton, Button, Typography, Container, Paper, Grid, Input, Slider } from '@material-ui/core'
+import VolumeUp from '@material-ui/icons/VolumeUp';
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    width: 250,
+  },
+  input: {
+    width: 42,
+  },
+})
+
+export default function App() {
+  const classes = useStyles();
+  const [engineer, setEngineer] = useState(0)
+
+  const handleSliderChange = (event, newValue) => {
+    setEngineer(newValue);
+  }
+
+  const handleInputChange = (event) => {
+    setEngineer(event.target.engineer === '' ? '' : Number(event.target.value))
+  }
+
+  const handleBlur = () => {
+    if (engineer < 0) {
+      setEngineer(0);
+    }
+  }
+
   return (
-    <AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" className='menuButton' color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" className='title'>
-      News
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
+    <>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Container maxWidth="md">
+            <Typography variant="h6" color="grey">
+              How Much Is This Meeting Costing Us?
+            </Typography>
+          </Container>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md">
+        <Paper className="mainBody">
+          Please Select How Many of each level:
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              Engineer:
+            </Grid>
+            <Grid item xs>
+              <Slider
+                value={typeof engineer === 'number' ? engineer : 0}
+                onChange={handleSliderChange}
+                aria-labelledby="input-slider"
+              />
+            </Grid>
+            <Grid item>
+              <Input
+                className={classes.input}
+                value={engineer}
+                margin="dense"
+                onChange={handleInputChange}
+                onBlur={handleBlur}
+                inputProps={{
+                  step: 1,
+                  min: 0,
+                  max: 100,
+                  type: 'number',
+                  'aria-labelledby': 'input-slider',
+                }}
+              />
+            </Grid>
+          </Grid>
+          {engineer}
+        </Paper>
+      </Container>
+    </>
   )
 }
-
-export default App
